91f631c232daf0a31c66f8f29ec27524bfb3ebdf
Lenar Minikaev <minikaev_lenar@mail.ru>
2023-07-08 06:07:16 +0300

    added integer scanner

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!\n");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n);
+    }
+}
