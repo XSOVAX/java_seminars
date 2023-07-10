be53d5852144b5259599033f3f82512dbd127801
Lenar Minikaev <minikaev_lenar@mail.ru>
2023-07-10 18:06:05 +0300

    Solved 1 task HW

---
+++ src/main/java/org/example/lesson1/HomeWork/Task1HW.java
@@ -0,0 +1,27 @@
+package org.example.lesson1.HomeWork;
+
+import java.util.Scanner;
+
+public class Task1HW {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String offer = scanner.nextLine();
+        System.out.println(ReverseWords(offer));
+    }
+    private static String ReverseWords(String s) {
+        String[] offer_array = s.split(" ");
+        int k = 0;
+        s = "";
+        for(int i = offer_array.length - 1; i >= 0; i--) {
+            if (offer_array[i].length() > 0){
+                if (k != 0) {
+                    s += " ";
+                }
+                s += offer_array[i];
+                k++;
+            }
+        }
+
+        return s;
+    }
+}
