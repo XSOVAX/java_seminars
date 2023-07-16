ef66142d30f79a344e29cb9553f8e074c4ed2c95
Lenar Minikaev <minikaev_lenar@mail.ru>
2023-07-16 22:07:52 +0300

    Solved Ex2 lesson 3

--- src/main/java/org/example/lesson3/ex2/Main.java
+++ src/main/java/org/example/lesson3/ex2/Main.java
@@ -1,2 +1,31 @@
-package org.example.lesson3.ex2;public class Main {
+package org.example.lesson3.ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.sum;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("Иванова", 23, 1000, List.of(3,4,5));
+        Student student2 = new Student("Семенов", 31, 1000, List.of(5,4,5));
+        Student student3 = new Student("Сидорова", 25,1500, List.of(5,4,5));
+        List<Student> students= new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        for (Student student : students) {
+            if (student.getFIO().endsWith("ова") && (sumScores(student.getScore()) % 2 == 0)){
+                System.out.println(student.getSalary());
+            }
+        }
+    }
+
+    private static Integer sumScores(List<Integer> score) {
+        int sum = 0;
+        for (Integer integer : score) {
+            sum += integer;
+        }
+        return sum;
+    }
 }
--- src/main/java/org/example/lesson3/ex2/Student.java
+++ src/main/java/org/example/lesson3/ex2/Student.java
@@ -1,2 +1,17 @@
-package org.example.lesson3.ex2;public class Student {
+package org.example.lesson3.ex2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String FIO;
+    private int numberGroup;
+    private int salary;
+    private List<Integer> score;
 }
